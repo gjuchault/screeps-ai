788f09a7e09d68fab47dd0c523593bc4fbfcb7eb
Gabriel Juchault <gabriel.juchault@gmail.com>
2019-09-07 17:24:56 +0200

    chore: initial toolchain

---
+++ rollup.config.js
@@ -0,0 +1,20 @@
+import clear from 'rollup-plugin-clear'
+import resolve from 'rollup-plugin-node-resolve'
+import commonjs from 'rollup-plugin-commonjs'
+import typescript from 'rollup-plugin-typescript2'
+
+export default {
+  input: 'src/index.ts',
+  output: {
+    file: 'dist/index.js',
+    format: 'cjs',
+    sourcemap: true
+  },
+
+  plugins: [
+    clear({ targets: ['dist'] }),
+    resolve(),
+    commonjs(),
+    typescript({ tsconfig: './tsconfig.json' }),
+  ]
+}
